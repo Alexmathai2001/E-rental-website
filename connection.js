84cc3c4ce7adeacb30ba402e87fd202790d07aa2
Alexmathai2001 <alexmathai07@gmail.com>
2024-01-31 16:09:46 +0530

    updated mongodb to env

--- connection.js
+++ connection.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose')
+require("dotenv").config()
 
 const connectdb = async () => {
   try {
-    const connection = await mongoose.connect('mongodb+srv://alexmathai07:Alexmathai@torqerentals.ftntmwz.mongodb.net/Torq?retryWrites=true&w=majority');
+    const connection = await mongoose.connect(process.env.MONGODB_CONNECT);
     console.log('MongoDB connected successfully!');
   } catch (error) {
     console.error('Error connecting to MongoDB:', error.message);
